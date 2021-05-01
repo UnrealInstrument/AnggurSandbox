73807cbc5adc0cfab59df766038b75187365f67a
wachid <wachid@fisip.net>
2021-05-02 05:40:19 +0700

    add pendulum class

--- Source/EntryPoint.cpp
+++ Source/EntryPoint.cpp
@@ -4,26 +4,88 @@
 using namespace Anggur;
 using R = Renderer;
 
+struct Pendulum
+{
+    Vector origin;
+    float size = 2;
+    float length = 30;
+    float angle = 0;
+    float angleAcc = 0;
+    float angleVel = 0;
+
+    float gravity = -9.8;
+    float damping = 0.999;
+
+    Color wire;
+    Color bob;
+
+    void Update(float dx)
+    {
+        angleAcc = Math::Cos(angle) * (gravity / length);
+        angleVel += angleAcc;
+        angleVel *= damping;
+        angle += angleVel * dx;
+    }
+
+    void Render()
+    {
+        Vector tip = Vector::CreatePolar(angle, length);
+        R::AddLine(origin, tip, 0.125, wire);
+        R::AddCircle(tip, size, bob);
+    }
+};
+
 class App: public Application
 {
     Camera camera;
 
+    bool run = false;
+
+    std::vector<Pendulum> pendulums;
+
     void OnInitialize() override
     {
         camera.SetRatio(mWindowConfig.GetRatio());
-        camera.Scale(Vector(0.01, 0.01));
+        camera.Scale(Vector(0.02, 0.02));
+        camera.Translate(Vector(0.0, 0.8));
+    }
+
+    void OnAttach() override
+    {
+        Color wireA = Color(0.7, 0.7, 0.7);
+        Color wireB = Color::white;
+        Color bobA = Color::Lerp(Color::purple, Color::black, 0.5);
+        Color bobB = Color::purple;
+
+        size_t total = 10;
+
+        for (size_t i = 0; i < total; ++i)
+        {
+            Pendulum p;
+            float amount = i / static_cast<float>(total);
+
+            p.size = 2 + (2 * amount);
+            p.length = 16 + (32 * amount);
+            p.wire = Color::Lerp(wireA, wireB, amount);
+            p.bob = Color::Lerp(bobA, bobB, amount);
+            pendulums.push_back(p);
+        }
     }
 
     void OnUpdate(float dx) override
     {
+        if (Input::IsKeyPressed(Key::Space)) run = !run;
+
         R::ClearBackground();
         R::BeginScene(camera);
 
-        R::AddCircle(Vector(0, 12), 10, Color::purple);
-        R::AddCircle(Vector(-12, -9), 10, Color::purple);
-        R::AddCircle(Vector(12, -9), 10, Color::purple);
+        for (Pendulum& p: pendulums)
+        {
+            if (run) p.Update(dx);
+            p.Render();
+        }
 
-        R::AddPolyring({Vector(0, 40), Vector(-35, -22), Vector(35, -22)}, 1);
+        R::AddCircle(Vector(0, 0), 1);
 
         R::EndScene();
     }
@@ -31,6 +93,6 @@ class App: public Application
 
 int main() 
 {
-	App app;
-	app.Run();
+        App app;
+        app.Run();
 }
